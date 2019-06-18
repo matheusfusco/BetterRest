162fea1d5bdcc8b9fb3e8388b6525997abccf3a3
Matheus <matheusfusco@gmail.com>
2019-06-18 10:18:52 -0300

    Layout da tela.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 struct ContentView : View {
     @State private var wakeUp = Date()
+    @State private var sleepAmount: Double = 8
+    @State private var coffeeAmount: Int = 1
     
     var body: some View {
         NavigationView {
@@ -19,7 +21,31 @@ struct ContentView : View {
                     .lineLimit(nil)
                 
                 DatePicker($wakeUp, displayedComponents: .hourAndMinute)
+                
+                Text("Quantidade desejada de sono")
+                    .font(.headline)
+                    .lineLimit(nil)
+                
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                }
+                
+                Text("Quantidade desejada de sono")
+                    .font(.headline)
+                    .lineLimit(nil)
+                
+                Stepper(value: $coffeeAmount, in: 1...20, step: 1) {
+                    if coffeeAmount == 1 {
+                        Text("1 cup")
+                    } else {
+                        Text("\(coffeeAmount) cups")
+                    }
+                }
+                
+                Spacer()
             }
+                .navigationBarTitle(Text("Better Rest"))
+            .padding()
         }
     }
 }
